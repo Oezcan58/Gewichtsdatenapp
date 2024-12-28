05be42c46858e6a850caa0151b9f2884da2cf39e
Oezcan58 <102741258+Oezcan58@users.noreply.github.com>
2024-12-28 05:06:46 +0100

    Update Werte.cs

--- Gewichtsdatenapp/Model/Werte.cs
+++ Gewichtsdatenapp/Model/Werte.cs
@@ -1,19 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace Gewichtsdatenapp.Model
 {
-    public class Werte
-
+    public class Werte : INotifyPropertyChanged
     {
-        public DateTime Date { get; set; }= DateTime.Now;
-        public double Height { get; set; }
-        public int Age { get; set; }
-        public double Weight { get; set; } // Gewicht
-        public string Gender {  get; set; }
-        public double BMI { get; set; } // Berechneter BMI
-    }
-}
+        private double _weight;
+        private double _height;
+        private double _bmi;
+        private string _gewichtsklasse;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        public double Weight
+        {
+            get => _weight;
+            set
+            {
+                if (_weight != value)
+                {
+                    _weight = value;
+                    OnPropertyChanged(nameof(Weight));
+                    BerechneBMI(); // Berechnet BMI automatisch
+                }
+            }
+        }
+
+        public double Height
+        {
+            get => _height;
+            set
+            {
+                if (_height != value)
+                {
+                    _height = value;
+                    OnPropertyChanged(nameof(Height));
+                    BerechneBMI(); // Berechnet BMI automatisch
+                }
+            }
+        }
+
+        public double BMI
+        {
+            get => _bmi;
+            private set
+            {
+                if (_bmi != value)
+                {
+                    _bmi = value;
+                    OnPropertyChanged(nameof(BMI));
+                }
+            }
+        }
+
+        public string Gewichtsklasse
+        {
+            get => _gewichtsklasse;
+            private set
+            {
+                if (_gewichtsklasse != value)
+                {
+                    _gewichtsklasse = value;
+                    OnPropertyChanged(nameof(Gewichtsklasse));
+                }
+            }
+        }
+
+        public int Age { get; set; } // Alter bleibt unverändert
+        public string Gender { get; set; } // Geschlecht bleibt unverändert
+
+        private void BerechneBMI()
+        {
+            if (Height > 0)
+            {
+                BMI = Math.Round(Weight / (Height * Height), 2);
+                Gewichtsklasse = BestimmeGewichtsklasse(BMI);
+            }
+        }
+
+        private string BestimmeGewichtsklasse(double bmi)
+        {
+            if (bmi < 18.5) return "Untergewicht";
+            if (bmi < 24.9) return "Normalgewicht";
+            if (bmi < 29.9) return "Übergewicht";
+            return "Adipositas";
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
